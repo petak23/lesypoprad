d88f2c11979da56d17e57e6ac03a6145f1218730
petak23-laptop <petak23@gmail.com>
2022-10-18 11:57:33 +0200

    0.5.16 - Oprava chyby: trLang

--- app/FrontModule/presenters/DokumentyPresenter.php
+++ app/FrontModule/presenters/DokumentyPresenter.php
@@ -1,9 +1,10 @@
 <?php
+
 namespace App\FrontModule\Presenters;
 
 /**
  * Prezenter pre smerovanie na dokumenty.
- * Posledna zmena(last change): 18.02.2022
+ * Posledna zmena(last change): 18.10.2022
  *
  *	Modul: FRONT
  *
@@ -11,29 +12,32 @@ namespace App\FrontModule\Presenters;
  * @copyright  Copyright (c) 2012 - 2022, Ing. Peter VOJTECH ml.
  * @license
  * @link       http://petak23.echo-msz.eu
- * @version 1.0.4
+ * @version 1.0.5
  */
-class DokumentyPresenter extends BasePresenter {
+class DokumentyPresenter extends BasePresenter
+{
 
   /** Vychodzie nestavenia */
-	protected function startup() {
+  protected function startup()
+  {
     parent::startup();
     // Kontrola ACL
     if (!$this->user->isAllowed($this->name, $this->action)) {
-      $this->flashRedirect('Homepage:', sprintf($this->trLang('base_nie_je_opravnenie'), $this->action), 'danger');
+      $this->flashRedirect('Homepage:', sprintf($this->texty_presentera->translate('base_nie_je_opravnenie'), $this->action), 'danger');
     }
-	}
-  
+  }
+
   /** Vychodzia akcia
    * @param int $id Id dokumentu na zobrazenie
    */
-	public function actionDefault($id) {
-		if (($dokument = $this->dokumenty->find($id)) === FALSE) {
-      $this->error($this->trLang('dokument_not_found')); 
-    } 
-		$dokument->update(array('pocitadlo'=>$dokument->pocitadlo + 1));
+  public function actionDefault($id)
+  {
+    if (($dokument = $this->dokumenty->find($id)) === FALSE) {
+      $this->error($this->texty_presentera->translate('dokument_not_found'));
+    }
+    $dokument->update(array('pocitadlo' => $dokument->pocitadlo + 1));
 
-		$this->redirectUrl("http://".$this->nazov_stranky."/".$dokument->subor);
-		exit;
-	}
+    $this->redirectUrl("http://" . $this->nazov_stranky . "/" . $dokument->subor);
+    exit;
+  }
 }
--- app/FrontModule/presenters/ErrorPresenter.php
+++ app/FrontModule/presenters/ErrorPresenter.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\FrontModule\Presenters;
 
 use Nette\Application as NA;
@@ -6,7 +7,7 @@ use Tracy\Debugger;
 
 /**
  * Prezenter pre smerovanie na chybove stranky.
- * Posledna zmena(last change): 18.02.2022
+ * Posledna zmena(last change): 18.10.2022
  *
  *	Modul: FRONT
  *
@@ -14,34 +15,33 @@ use Tracy\Debugger;
  * @copyright  Copyright (c) 2012 - 2022 Ing. Peter VOJTECH ml.
  * @license
  * @link       http://petak23.echo-msz.eu
- * @version 1.0.4
+ * @version 1.0.5
  *
  */
-class ErrorPresenter extends \App\FrontModule\Presenters\BasePresenter {
-	
+class ErrorPresenter extends BasePresenter
+{
+
 	/**
 	 * @param  Exception
 	 * @return void
 	 */
-	public function renderDefault($exception) {
+	public function renderDefault($exception)
+	{
 		if ($this->isAjax()) { // AJAX request? Just note this error in payload.
 			$this->payload->error = TRUE;
 			$this->terminate();
-
 		} elseif ($exception instanceof NA\BadRequestException) {
 			$code = $exception->getCode();
-      $code_a = in_array($code, array(403, 404, 405, 410, 500)) ? $code : '4xx';
-      $this->template->h2 = $this->trLang('err_'.$code_a.'_h2');
-      $this->template->text = $this->trLang('err_'.$code_a.'_text');
-      $this->template->err_code = "error ".$code_a;
-			$this->setView($code==500 ? "500" : "400");
+			$code_a = in_array($code, array(403, 404, 405, 410, 500)) ? $code : '4xx';
+			$this->template->h2 = $this->texty_presentera->translate('err_' . $code_a . '_h2');
+			$this->template->text = $this->texty_presentera->translate('err_' . $code_a . '_text');
+			$this->template->err_code = "error " . $code_a;
+			$this->setView($code == 500 ? "500" : "400");
 			// log to access.log
 			Debugger::log("HTTP code $code: {$exception->getMessage()} in {$exception->getFile()}:{$exception->getLine()}", 'access');
-
 		} else {
 			$this->setView('500'); // load template 500.latte
 			Debugger::log($exception, Debugger::ERROR); // and log exception
 		}
 	}
-
 }
